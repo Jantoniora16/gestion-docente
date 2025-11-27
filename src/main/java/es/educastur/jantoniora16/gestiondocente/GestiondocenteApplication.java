cb09dbab77a4ceb22af2271f0237a353d0764a97
tony2 <tony2@Joseiro.home>
2025-11-27 09:10:22 +0100

    Configuración inicial del proyecto

---
+++ src/main/java/es/educastur/jantoniora16/gestiondocente/GestiondocenteApplication.java
@@ -0,0 +1,13 @@
+package es.educastur.jantoniora16.gestiondocente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondocenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiondocenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/educastur/jantoniora16/gestiondocente/GestiondocenteApplicationTests.java
@@ -0,0 +1,13 @@
+package es.educastur.jantoniora16.gestiondocente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondocenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
